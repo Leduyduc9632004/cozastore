e48a042154804abbc51be1b33c36bd9484c7158d
Leduyduc9632004 <leduyduc963@gmail.com>
2024-07-14 22:24:36 +0700

    tổ chức thư mục dự án

---
+++ database/migrations/2024_07_14_103855_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Color;
+use App\Models\Size;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Category::class)->constrained();
+            $table->foreignIdFor(Size::class)->constrained();
+            $table->foreignIdFor(Color::class)->constrained();
+            $table->string('name');
+            $table->string('image');
+            $table->double('price');
+            $table->longText('description');
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
